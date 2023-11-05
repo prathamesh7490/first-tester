9055625deac923afc23e03b118f86a9ef1152cc8
prathamesh7490 <prathamr15@gmail.com>
2023-11-05 09:39:24 +0530

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+import javax.xml.transform.stream.StreamSource;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 9;
+        if (x % 2 ==0) {
+
+            System.out.println("Number is even");
+
+        } else {
+            System.out.println("Number is odd");
+        }
+
+    }
+}
+
